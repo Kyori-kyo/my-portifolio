3ee89f1a03d6339616784f46664e81a1c291280e
Ríchard Lucas <83315499+Kyori-kyo@users.noreply.github.com>
2023-07-08 15:49:56 -0300

    revamped the contact page

--- src/app/Pages/contact/page.tsx
+++ src/app/Pages/contact/page.tsx
@@ -1,71 +1,40 @@
-'use client'
-import { useForm } from "react-hook-form";
-
 type Props = {}
 
 const Contact = (props: Props) => {
 
-
-    const axios = require('axios');
-
-    async function sendEmail(email : string, subject : string, message : string) {
-    const data = JSON.stringify({
-        "Messages": [{
-        "From": {"Email": email},
-        "To": [{"Email": 'richardlcmoreira@gmail.com'}],
-        "assunto": subject,
-        "message": message
-        }]
-    });
-
-    const config = {
-        method: 'post',
-        url: 'https://api.mailjet.com/v3.1/send',
-        data: data,
-        headers: {'Content-Type': 'application/json'},
-        auth: {username: '<API Key>', password: '<Secret Key>'},
-    };
-
-    return axios(config)
-        .then(function (response : any) {
-        console.log(JSON.stringify(response.data));
-        })
-        .catch(function (error : any) {
-        console.log(error);
-        });
-
-    }
-    
-    // form handler
-    const {register, handleSubmit} = useForm();
-    const onSubmit = (event: any) => {
-        sendEmail(event["email"], event["assunto"], event["message"]);
-    }
-
     return (
         <div className={'flex justify-center w-screen'} id="pog">
             <div className={'flex flex-col gap-10 justify-center items-center max-w-screen-xl w-full glassMorphDiv px-10 py-16 rounded-3xl'}>
-                <h1 className={'text-4xl m-5 border-b-2 border-spacing-x-0.5 border-orange-500'}>
+                <h1 className={'text-4xl border-b-2 border-spacing-x-0.5 border-orange-500'}>
                     CONTACT
                 </h1>
-                <form onSubmit={handleSubmit(onSubmit)} method="post" className={'flex flex-col gap-5'}>
-                    
-                    <div className={'flex content-center justify-center'}>
-                        <div className={'flex flex-col text-right gap-5'}>
-                            <label className={'text-base p-1 pr-3'}>Email:</label>
-                            <label className={'text-base p-1 pr-3'}>Assunto:</label>
-                            <label className={'text-base p-1 pr-3'}>Mensagem:</label>
-                        </div>
+                <p>
+                    If you have a question 
+                    <br/> 
+                    or just want to get in touch
+                    <br/> 
+                    <br/> 
+                    feel free to send me an <a className={'text-orange-500 underline hover:scale-50'} href="mailto:richardlcmoreira@gmail.com">e-mail</a>
+                    <br/> 
+                    or contact me on my social media
+                </p>
+                <div className={'absolute -bottom-36'}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 72 72" width="72" height="72" preserveAspectRatio="xMidYMid meet" className={'slide-top'}>
+                        <g clip-path="url(#__lottie_element_2)">
+                            
+                            <g transform="matrix(1,0,0,1,36,36)" opacity="1" className='block'>
+                                <path stroke-linecap="butt" stroke-linejoin="miter"  fill-opacity="0" stroke-miterlimit="4" stroke="rgb(207,207,207)" stroke-opacity="1" stroke-width="2" d=" M18.323999404907227,4.427999973297119 C18.323999404907227,4.427999973297119 18.28700065612793,14.213000297546387 18.28700065612793,14.213000297546387 C18.28700065612793,24.312999725341797 10.100000381469727,32.5 0,32.5 C0,32.5 0,32.5 0,32.5 C-10.100000381469727,32.5 -18.28700065612793,24.312999725341797 -18.28700065612793,14.213000297546387 C-18.28700065612793,14.213000297546387 -18.28700065612793,-14.213000297546387 -18.28700065612793,-14.213000297546387 C-18.28700065612793,-24.312999725341797 -10.100000381469727,-32.5 0,-32.5 C0,-32.5 0,-32.5 0,-32.5 C10.100000381469727,-32.5 18.28700065612793,-24.312999725341797 18.28700065612793,-14.213000297546387 C18.28700065612793,-14.213000297546387 18.28700065612793,7.375999927520752 18.28700065612793,7.375999927520752"></path></g><g className='none' transform="matrix(1,0,0,1,36,36)" opacity="1"><path stroke-linecap="butt" stroke-linejoin="miter" fill-opacity="0" stroke-miterlimit="4" stroke="rgb(207,207,207)" stroke-opacity="1" stroke-width="2" d="M0 0"></path></g><g transform="matrix(1,0,0,1,36,36)" opacity="1" className='block'><path stroke-linecap="butt" stroke-linejoin="miter" fill-opacity="0" stroke-miterlimit="4" stroke="rgb(249,115,22)" stroke-opacity="1" stroke-width="2" d="M0 0"></path></g><g className='none' transform="matrix(1,0,0,1,36,36)" opacity="1"><path stroke-linecap="butt" stroke-linejoin="miter" fill-opacity="0" stroke-miterlimit="4" stroke="rgb(249,115,22)" stroke-opacity="1" stroke-width="2" d="M0 0"></path></g><g className='block' transform="matrix(1,0,0,1,36,43.97333526611328)" opacity="1"><path stroke-linecap="butt" stroke-linejoin="miter" fill-opacity="0" stroke-miterlimit="4" stroke="rgb(249,115,22)" stroke-opacity="1" stroke-width="2" d=" M-0.10599999874830246,2.9800000190734863 C0.003000000026077032,4.315000057220459 0.06499999761581421,5.785999774932861 0.06499999761581421,7.4120001792907715 C0.06499999761581421,7.4120001792907715 0.006000000052154064,16.625999450683594 0.006000000052154064,16.625999450683594">
+                                </path>
+                            </g>
 
-                        <div className={'flex flex-col text-left gap-5 text-black'}>
-                            <input className={'text-base pl-4 rounded-md w-full min-w-[20rem] p-1 max-h-96 border border-white'} type="text" id="email"   {...register("email")} placeholder="...@email.com" />
-                            <input className={'text-base pl-4 rounded-md w-full min-w-[20rem] p-1 max-h-96 border border-white'} type="text" id="assunto" {...register("assunto")} placeholder="Olá! Estou te contatando para..." />
-                            <textarea className={'text-base pl-4 rounded-md w-full min-w-[20rem] p-1 max-h-96 border border-white min-h-[8rem]'} id="message" {...register("message")} placeholder="Escreva sua mensagem aqui..."/>
-                            <input className={'bg-slate-500 hover:scale-110'} value="submit" type="submit" />
-                        </div>
-                    </div>
-
-                </form>
+                            <g className='block' transform="matrix(1,0,0,1,54,52.22333526611328)" opacity="1">
+                                <path stroke-linecap="butt" stroke-linejoin="miter" fill-opacity="0" stroke-miterlimit="4" stroke="rgb(249,115,22)" stroke-opacity="1" stroke-width="2" d=" M-24.45199966430664,3.003000020980835 C-24.45199966430664,3.003000020980835 -18.042999267578125,9.496999740600586 -18.042999267578125,9.496999740600586 C-18.042999267578125,9.496999740600586 -11.54800033569336,3.003000020980835 -11.54800033569336,3.003000020980835">
+                                </path>
+                            </g>
+                        </g>
+                    </svg>
+                </div>
+                
             </div>
         </div>
     )
