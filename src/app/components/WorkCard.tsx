5bc25bf50356b75388036a2db26120e558e82b4e
Ríchard Lucas <83315499+Kyori-kyo@users.noreply.github.com>
2023-07-12 14:15:39 -0300

    work card adjustments

--- src/app/components/WorkCard.tsx
+++ src/app/components/WorkCard.tsx
@@ -1,18 +1,19 @@
 import Image from 'next/image';
 
 type Props = {
+    urlLink: string;
     langs: string[];
     titulo: string;
     imagem: string;
     descricao: string;
 };
 
-const WorkCard = ({ langs, titulo, imagem, descricao }: Props) => {
+const WorkCard = ({ langs, titulo, imagem, descricao, urlLink }: Props) => {
     return (
-        <div className="relative bg-cover text-gray-200 max-w-[350px] min-w-[350px] max-h-[350px] min-h-[350px] tracking-wide group overflow-hidden rounded-2xl">
+        <a href={urlLink} target='_blank' className="relative bg-cover text-gray-200 max-w-[350px] min-w-[350px] max-h-[350px] min-h-[350px] tracking-wide group overflow-hidden rounded-2xl">
             
-            <div className="h-full flex items-end bg-gradient-to-t from-black/90 duration-500 translate-y-full group-hover:translate-y-0 bg-transparent absolute inset-0 z-10">
-                <div className="pb-2 px-6 rounded-[0px] text-left">
+            <div className="max-w-[350px] min-w-[350px] h-full flex items-end bg-gradient-to-t from-black/90 duration-500 translate-y-full group-hover:translate-y-0 bg-transparent absolute inset-0 z-10">
+                <div className="pb-2 px-6 rounded-[0px] text-left max-w-[350px] min-w-[350px]">
                     <h1 className="inline text-lg font-semibold border-b-2 border-orange-500">{titulo}</h1>
                     <p className="text-base pb-2 pt-2">{descricao}</p>
 
@@ -35,7 +36,7 @@ const WorkCard = ({ langs, titulo, imagem, descricao }: Props) => {
                 width={350}
                 className="pointer-events-none"
             />
-        </div>
+        </a>
     );
 };
 
