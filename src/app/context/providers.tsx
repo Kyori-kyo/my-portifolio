d20fd487a62363b4fd024ea33db14a8dd7ee2eae
Ríchard Lucas <83315499+Kyori-kyo@users.noreply.github.com>
2023-08-13 12:17:16 -0300

    cleanned unused providers

--- src/app/context/providers.tsx
+++ src/app/context/providers.tsx
@@ -1,11 +1,1 @@
-'use client'
-import { ChakraProvider } from '@chakra-ui/react'
-type Props = { children: React.ReactNode }
-
-export const Providers = ({ children }: Props) => {
-    return (
-        <ChakraProvider>
-            {children}
-        </ChakraProvider>
-    );
-}
+// 
