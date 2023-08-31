6c87289e3c89670c4094dfbe6b90e2bfd8da269a
Ríchard Lucas <83315499+Kyori-kyo@users.noreply.github.com>
2023-08-31 09:59:40 -0300

    updated SEO

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,13 +2,47 @@ import '@/app/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Providers } from '../context/providers'
+import { GenerateMetadata } from '@/libs/types/metadata'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: "Ríchard's Portfolio",
-  description: 'My personal portfolio website',
-}
+export const generateMetadata: GenerateMetadata = () => {
+  return {
+    title: {
+      default: "Ríchard's Portfolio",
+      template: "%s | Ríchard's Portfolio",
+    },
+    description: "This is my portifolio, and it tells about me and my career.",
+    keywords:
+      "Ríchard, Portifolio, Ríchard's Portifolio, Ríchard Lucas, Kyori, Kyori kyo, Ríchard Lucas, Ríchard Lucas",
+    viewport: "width=device-width, initial-scale=1",
+    creator: "Ríchard Lucas",
+    publisher: "Ríchard Lucas",
+    authors: [{ name: "Ríchard Lucas" }],
+    robots: "index, follow",
+    themeColor: [
+      { media: "(prefers-color-scheme: light)", color: "white" },
+      { media: "(prefers-color-scheme: dark)", color: "black" },
+    ],
+    openGraph: {
+      type: "website",
+      locale: "en",
+      siteName: "Ríchard's Portifolio",
+      url: "https://richard-lucas.vercel.app/assets/images/og.png",
+      title: "Ríchard's Portifolio",
+      description:
+        "This is my portifolio, and it tells about me and my career.",
+      images: [
+        {
+          url: "https://richard-lucas.vercel.app/",
+          width: 1280,
+          height: 720,
+          alt: "Ríchard's Portifolio",
+        },
+      ],
+    },
+  };
+};
 
 export default function RootLayout({
   children,
